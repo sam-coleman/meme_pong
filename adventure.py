06620f0cb8c42124c10ae08f38bc93aaf89c6728
Hazel Smith <hsmith1@olin.edu>
2020-04-29 14:50:35 -0500

    MVP adventure game

--- adventure.py
+++ adventure.py
@@ -13,22 +13,27 @@ import math
 import time
 import numpy as np
 
-SCREEN_WIDTH = 1000
+MAP_HEIGHT = 2400
+MAP_WIDTH = 1000
+
+SCREEN_WIDTH = MAP_WIDTH
 SCREEN_HEIGHT = 600
 
+
 class Person(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         """
         """
         super(Person, self).__init__()
         self.surf = pygame.image.load("images/standing.png").convert()
-        self.rect = self.surf.get_rect(center = (25,SCREEN_HEIGHT-50))
+        self.rect = self.surf.get_rect(center = (x,y))
         self.x=self.rect.x
         self.y=self.rect.y
         self.vx=0
         self.vy=0
         self.ax=0
         self.ay=0
+        self.on_platform = False
 
     def jump(self):
         """
@@ -41,13 +46,15 @@ class Person(pygame.sprite.Sprite):
         """
         Makes person walk
         """
-        self.vx=direction/2
+        self.x+=direction
 
     def is_touching_ground(self):
         """
         Checks if person is standing or in free fall
         """
-        if self.rect.bottom==SCREEN_HEIGHT:
+        if self.rect.bottom==MAP_HEIGHT:
+            return True
+        elif self.on_platform == True:
             return True
         else:
             return False
@@ -56,33 +63,35 @@ class Person(pygame.sprite.Sprite):
         """
         Changes acceleration of the Person
         """
-        if self.is_touching_ground()==False:
-            self.ay=.005
-        else:
+        if self.is_touching_ground()==True:
             self.ay=0
-
-        if self.vx!=0:
-            self.ax=1*-np.sign(self.vx)
         else:
-            self.ax=0
+            self.ay=.005
 
-
-    def keep_on_screen(self):
+    def keep_on_map(self):
         """
         Ensures that the person stays on screen
         """
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        else:
-            self.rect.y += 1
+        # if self.rect.top <= 0:
+        #     self.rect.top = 0
 
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+        if self.rect.bottom >= MAP_HEIGHT:#stops person faling through floor
+            self.vy=0
+            self.rect.bottom=MAP_HEIGHT
 
         if self.rect.left <= 0:
             self.rect.left = 0
-        elif self.rect.right >= SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
+
+        elif self.rect.right >= MAP_WIDTH:
+            self.rect.right = MAP_WIDTH
+
+    def is_on_platform(self,platform):
+        """
+        Checks if Person is on platform
+        """
+        if self.rect.bottom == platform.rect.top and self.rect.right<=platform.rect.right and self.rect.left>=platform.rect.left:
+            self.vy=0
+            self.on_platform=True
 
     def update(self, pressed_keys):
         """Updates the postion of the player"""
@@ -93,37 +102,83 @@ class Person(pygame.sprite.Sprite):
         if pressed_keys[K_LEFT]:
             self.walk(-1)
 
-
-        self.x+=self.vx
-        self.y+=self.vy
-        self.vx+=self.ax
         self.vy+=self.ay
-
+        self.y+=self.vy
         self.accelerate()
 
         #move ball to current x and y values
         self.rect.x = self.x
         self.rect.y = self.y
 
-        self.keep_on_screen()
+        #move ball to current x and y values
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.keep_on_map()
+        self.on_platform=False #reset on_platform
 
 
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super(Platform, self).__init__()
+        self.width = 100
+        self.height = 25
+        self.surf = pygame.Surface((self.width, self.height))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center = (x,y))
+
+# class Screen():
+#     def __init__(self, width, height,top, left ):
+#         self.width= width
+#         self.height= height
+#         self.top=top
+#         self.left=left
+
+class Map():
+    def __init__(self,platforms):
+        self.width=MAP_WIDTH
+        self.height=MAP_HEIGHT
+        self.platforms=platforms
+        self.screen_top=MAP_HEIGHT-SCREEN_HEIGHT
+        self.screen_left=0
+        self.screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    def draw(self):
+        for platform in self.platforms:
+            r = Rect(platform.rect.left, platform.rect.top-self.screen_top, platform.rect.width, platform.rect.height)
+            screen.blit(platform.surf, r)
 
 if __name__ == '__main__':
 
     # Initialize pygame
     pygame.init()
-
+    print("running")
     # Create the screen object
     # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    person= Person()
+    person= Person(25,MAP_HEIGHT-50)
+
+    platform1=Platform(MAP_WIDTH/4,MAP_HEIGHT-1950)
+    platform2=Platform(MAP_WIDTH/2,MAP_HEIGHT-100)
+    platform3=Platform(3/4*MAP_WIDTH,MAP_HEIGHT-450)
+
     all_sprites = pygame.sprite.Group()
     all_sprites.add(person)
+    all_sprites.add(platform1)
+    all_sprites.add(platform2)
+    all_sprites.add(platform3)
+
+    platform_group = pygame.sprite.Group()
+    platform_group.add(platform1)
+    platform_group.add(platform2)
+    platform_group.add(platform3)
+
+    map=Map(platform_group)
 
     running = True
     while running:
-
+        #screen.bottom=person.rect.bottom
         # Look at every event in the queue
         for event in pygame.event.get():
             # Did the user hit a key?
@@ -139,10 +194,23 @@ if __name__ == '__main__':
         pressed_keys = pygame.key.get_pressed()
         person.update(pressed_keys)
 
+        for platform in map.platforms:
+            person.is_on_platform(platform)
+
+
         # Fill the screen with black
         screen.fill((0, 0, 0))
 
-        for entity in all_sprites:
-            screen.blit(entity.surf, entity.rect)
+        map.draw()
+        r=Rect(person.rect.left, person.rect.top-map.screen_top, person.rect.width, person.rect.height)
+        map.screen.blit(person.surf, r)
 
-        pygame.display.flip()
+
+        map.screen_top=min(MAP_HEIGHT-SCREEN_HEIGHT,person.rect.bottom-SCREEN_HEIGHT/2)
+
+
+        # for entity in all_sprites:
+        #     screen.blit(entity.surf, entity.rect)
+
+        time.sleep(.001)
+        pygame.display.update()
