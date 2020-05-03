821a69d1b44a6590e53333380d7db6d393d363dc
Hazel Smith <hsmith1@olin.edu>
2020-05-03 12:03:34 -0500

    added docstrings to adventure.py

--- adventure.py
+++ adventure.py
@@ -22,7 +22,13 @@ SCREEN_HEIGHT = 600
 
 class Person(pygame.sprite.Sprite):
     def __init__(self,x,y):
-        """
+        """Initialize a Person
+
+        Init function creates a person object.
+
+        args:
+            x=person's starting x position
+            y=person's starting y position
         """
         super(Person, self).__init__()
         self.surf = pygame.image.load("images/Person.png").convert()
@@ -37,15 +43,17 @@ class Person(pygame.sprite.Sprite):
         self.has_bike=False
 
     def jump(self):
-        """
-        Makes person jump
+        """Makes person jump
+
+        Makes person jump by checking if a person in on a surface
+        and then changing the y velocity
         """
         if self.is_touching_ground()==True: #or touching platform
             self.vy=-1.5
 
     def walk(self,direction):
         """
-        Makes person walk
+        Makes person walk by changing x position
         """
         self.x+=direction
 
@@ -62,7 +70,7 @@ class Person(pygame.sprite.Sprite):
 
     def accelerate(self):
         """
-        Changes acceleration of the Person
+        Changes acceleration of the Person based on if person is touching the gorund or not
         """
         if self.is_touching_ground()==True:
             self.ay=0
@@ -71,7 +79,7 @@ class Person(pygame.sprite.Sprite):
 
     def keep_on_map(self):
         """
-        Ensures that the person stays on screen
+        Ensures that the person stays in the map
         """
         # if self.rect.top <= 0:
         #     self.rect.top = 0
@@ -88,13 +96,20 @@ class Person(pygame.sprite.Sprite):
 
     def is_on_platform(self,platform):
         """
-        Checks if Person is on platform
+        Checks if Person is on platform and if so stops the person from falling
         """
         if self.rect.bottom == platform.rect.top and self.rect.right<=platform.rect.right and self.rect.left>=platform.rect.left:
             self.vy=0
             self.on_platform=True
 
     def collides_with_bike(self,bike):
+        """
+        Checks if Person is touching bike and if so changes the person's aperance
+
+        args:
+            bike=bike object to check if the person has collided with
+
+        """
         if self.rect.colliderect(bike.rect):
             self.surf = pygame.image.load("images/Person_on_Bike.png").convert()
             self.rect = self.surf.get_rect(center = (self.x,self.y))
@@ -102,7 +117,10 @@ class Person(pygame.sprite.Sprite):
             self.has_bike=True
 
     def update(self, pressed_keys):
-        """Updates the postion of the player"""
+        """Updates the postion of the person
+
+        Updates the position of the person based on what keys were pressed_keys
+        """
         if pressed_keys[K_UP]:
             self.jump()
         if pressed_keys[K_RIGHT]:
@@ -126,12 +144,24 @@ class Person(pygame.sprite.Sprite):
         self.on_platform=False
 
     def draw(self,map):
+        """Draws person on screen
+
+        Draws person on screen by calculating screen position based on map position.
+        """
         r=Rect(self.rect.left, self.rect.top-map.screen_top, self.rect.width, self.rect.height)
         map.screen.blit(self.surf, r)
 
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self,x,y):
+        """Initialize a Platform
+
+        Init function creates a platform object.
+
+        args:
+            x=platform's starting x position
+            y=platform's starting y position
+        """
         super(Platform, self).__init__()
         self.width = 100
         self.height = 25
@@ -142,6 +172,14 @@ class Platform(pygame.sprite.Sprite):
 
 class Map():
     def __init__(self,platforms,bike):
+        """Initialize a Map
+
+        Init function creates a map object.
+
+        args:
+            platform=pygame group of platforms in map
+            bike=a bike object
+        """
         self.width=MAP_WIDTH
         self.height=MAP_HEIGHT
         self.platforms=platforms
@@ -151,6 +189,11 @@ class Map():
         self.bike=bike
 
     def draw(self):
+        """Draws map on screen
+
+        Draws map on screen by calculating screen position of platforms and bike
+        based on their map position.
+        """
         for platform in self.platforms:
             r = Rect(platform.rect.left, platform.rect.top-self.screen_top, platform.rect.width, platform.rect.height)
             screen.blit(platform.surf, r)
@@ -161,7 +204,12 @@ class Map():
 
 class Bike(pygame.sprite.Sprite):
     def __init__(self,platform):
-        """
+        """Initialize a Bike
+
+        Init function creates a bike object.
+
+        args:
+            platform=platform object that bike should be on
         """
         super(Bike, self).__init__()
         self.surf = pygame.image.load("images/Bike.png").convert()
